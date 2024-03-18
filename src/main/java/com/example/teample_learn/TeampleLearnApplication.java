e696f62fa1b430106c29da43af5d02bb7ea2778c
kkkkimtaehyeon <kth1927@naver.com>
2024-03-19 01:30:03 +0900

    initial

--- src/main/java/com/example/teample_learn/TeampleLearnApplication.java
+++ src/main/java/com/example/teample_learn/TeampleLearnApplication.java
@@ -2,8 +2,9 @@ package com.example.teample_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TeampleLearnApplication {
 
     public static void main(String[] args) {
