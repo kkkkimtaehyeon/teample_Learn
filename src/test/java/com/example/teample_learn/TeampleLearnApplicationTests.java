5a4b9dcb8dfcbec9de03a89afba34a5f3314f3d9
kkkkimtaehyeon <kth1927@naver.com>
2024-03-18 18:18:23 +0900

    Initial commit

---
+++ src/test/java/com/example/teample_learn/TeampleLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teample_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeampleLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
