40f81f0aad16f05a32f14277103b9e89848acf3a
kkkkimtaehyeon <kth1927@naver.com>
2024-03-23 01:32:09 +0900

    (BE) PostUpdateRequestDto 수정

--- src/main/java/com/example/teample_learn/post/dto/PostUpdateRequestDto.java
+++ src/main/java/com/example/teample_learn/post/dto/PostUpdateRequestDto.java
@@ -8,24 +8,35 @@ import lombok.NoArgsConstructor;
 @Data
 public class PostUpdateRequestDto {
 
+    String author;
     String title;
     String content;
-    String category;
-    String requiredSkills;
     String tags;
-    Boolean contact;
+    Boolean meeting;
+    String category;
     String className;
     int classDivision;
+    String deadline;
+    String duration;
+    int quota;
+    String skills;
+    String contact;
 
     @Builder
-    public PostUpdateRequestDto (String title, String content, String category, String requiredSkills, String tags, Boolean contact, String className, int classDivision) {
+    public PostUpdateRequestDto(String title, String content, String tags, Boolean meeting, String category, String className,
+                              int classDivision, String deadline, String duration, int quota, String skills, String contact) {
+        this.author = author;
         this.title = title;
         this.content = content;
-        this.category = category;
-        this.requiredSkills = requiredSkills;
         this.tags = tags;
-        this.contact = contact;
+        this.meeting = meeting;
+        this.category = category;
         this.className = className;
         this.classDivision = classDivision;
+        this.deadline = deadline;
+        this.duration = duration;
+        this.quota = quota;
+        this.skills = skills;
+        this.contact = contact;
     }
 }
