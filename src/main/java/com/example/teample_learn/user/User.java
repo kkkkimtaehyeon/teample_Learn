3605635223adb7627bc0f159896b5fc142deafa7
kkkkimtaehyeon <kth1927@naver.com>
2024-03-30 21:44:12 +0900

    (BE) User 연관관계 수정

--- src/main/java/com/example/teample_learn/user/User.java
+++ src/main/java/com/example/teample_learn/user/User.java
@@ -1,5 +1,6 @@
 package com.example.teample_learn.user;
 
+import com.example.teample_learn.resume.domain.Resume;
 import com.example.teample_learn.teamplay.domain.BaseTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,33 +22,46 @@ public class User extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(name = "email", nullable = false)
     private String email;
 
-    @Column
+    @Column(name = "picture", nullable = true)
     private String picture;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
 
+    /*@OneToOne
+    @JoinColumn(name = "resume_id")
+    private Resume resume;*/
+
+    @OneToOne(mappedBy = "user")
+    public Resume resume;
+
     @Builder
-    public User(String name, String email, String picture, Role role) {
+    public User(String name, String email, String picture, Role role, Resume resume) {
         this.name = name;
         this.email = email;
         this.picture = picture;
         this.role = role;
+        this.resume = resume;
     }
 
     public User update(String name, String picture) {
         this.name = name;
         this.picture = picture;
         return this;
+    }
+
+    public void updateResume(Resume resume) {
+        this.resume = resume;
     }
 
     public String getRoleKey() {
